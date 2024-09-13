7fb31ed1dac308df20d8e5cdb875988d3b3d3e68
isaacforeal <isaacngabirano1@gmail.com>
2024-09-13 14:37:25 +0300

    nav bar position fixedddddd

--- components/component/about.jsx
+++ components/component/about.jsx
@@ -128,7 +128,7 @@ export default function About() {
           </Button> */}
         </div>
       </header>
-      <main className="flex-1 main-section pt-4">
+      <main className="flex-1 main-section mt-4">
         <section className={`w-full py-12 md:py-24 lg:py-32 section1`}>
           <div className="container px-4 md:px-6 grid gap-6 lg:grid-cols-2 lg:gap-12">
             <div className="flex flex-col justify-center space-y-4">
